4f6c75a2ad0ac9073a370dd950356bafba657c8d
sohlost <f20230343@goa.bits-pilani.ac.in>
2024-08-31 00:08:31 +0530

    sd

--- ProjectAssignment/Level2/rungpt2.py
+++ ProjectAssignment/Level2/rungpt2.py
@@ -7,13 +7,13 @@ model = GPT2LMHeadModel.from_pretrained(model_name)
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 
 
-input_text = "CEL BITS GOA is an organisation that"
+input_text = "My name is Soham and I want to join DaSH Labs because I find these topics fascinating and"
 
 input_ids = tokenizer.encode(input_text, return_tensors="pt")
 attention_mask = torch.ones(input_ids.shape, dtype=torch.long)
 
 
-output = model.generate(input_ids = input_ids, attention_mask = attention_mask, do_sample=True, max_length=500, top_k=50, top_p=0.95, temperature=0.9,pad_token_id = tokenizer.eos_token_id,  )
+output = model.generate(input_ids = input_ids, attention_mask = attention_mask, do_sample=True, max_length=100, top_k=50, top_p=0.95, temperature=0.9,pad_token_id = tokenizer.eos_token_id,  )
 
 generated_output = tokenizer.decode(output[0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
 print(generated_output)
