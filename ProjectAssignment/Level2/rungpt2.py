5133170cb4b447afb32c519886b0d0a90224a036
sohlost <f20230343@goa.bits-pilani.ac.in>
2024-08-30 21:45:10 +0530

    ads

---
+++ ProjectAssignment/Level2/rungpt2.py
@@ -0,0 +1,19 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import torch
+
+
+model_name = "gpt2"
+model = GPT2LMHeadModel.from_pretrained(model_name)
+tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+
+
+input_text = "CEL BITS GOA is an organisation that"
+
+input_ids = tokenizer.encode(input_text, return_tensors="pt")
+attention_mask = torch.ones(input_ids.shape, dtype=torch.long)
+
+
+output = model.generate(input_ids = input_ids, attention_mask = attention_mask, do_sample=True, max_length=500, top_k=50, top_p=0.95, temperature=0.9,pad_token_id = tokenizer.eos_token_id,  )
+
+generated_output = tokenizer.decode(output[0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
+print(generated_output)
